7ef38d7906a611ca289d62e7c5381a5e6800567c
Shivank Singhal <shivank.singhal.970@gmail.com>
2021-03-16 14:26:35 +0530

    my first commit

---
+++ src/main/java/com/thales/calculator/CalculatorRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thales.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorRestServiceApplication.class, args);
+	}
+
+}
